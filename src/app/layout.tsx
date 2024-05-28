65996275aae329115b75e3719ad13c014672e038
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2024-05-28 07:51:22 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,30 +4,34 @@ import { Providers } from "./providers";
 import { ReactNode } from "react";
 import NextAuSessionProvider from "./components/providers/session_provaiders";
 import PublicPageProvider from "./components/providers/publicPage";
-
+import { Box } from "@chakra-ui/react";
 
 export const metadata: Metadata = {
-  title: "Assinador Front",
-  description: "Assinador de documentos online"
+    title: "Assinador Front",
+    description: "Assinador de documentos online",
 };
 
 interface RootLayoutProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 export default async function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <html
-      lang="pt-br"
-      style={{ fontSize: "0.9rem", width: "100vw", height: "100vh" }}
-    >
-      <NextAuSessionProvider>
-        <PublicPageProvider>
-          <body className={fonts.rubik.variable}>
-            <Providers>{children}</Providers>
-          </body>
-        </PublicPageProvider>
-      </NextAuSessionProvider>
-    </html>
-  );
+    return (
+        <html
+            lang="pt-br"
+            style={{ fontSize: "0.7rem", width: "100vw", height: "100vh" }}
+        >
+            <NextAuSessionProvider>
+                <PublicPageProvider>
+                    <Box 
+                    // fontSize={{base: '', sm: '', md: "", xl: ''}}
+                    >
+                        <body className={fonts.rubik.variable}>
+                            <Providers>{children}</Providers>
+                        </body>
+                    </Box>
+                </PublicPageProvider>
+            </NextAuSessionProvider>
+        </html>
+    );
 }
